7a41b22eb014ad8d5d055c8cf2b1b2cb10977427
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-27 15:50:58 +0100

    fix fetch moved in for loop

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 /* Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
  */
 
-const emailSpaceEl = document.querySelector('.email_space');
-const emailListEl = document.querySelector('email_list');
-const emailEl = document.querySelector('.email');
+const emailListEl = document.querySelector('.email_list');
 
 
 
-fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+for (let i = 0; i < 10; i++) {
+    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        console.log(data.response);
+        
+        emailListEl.innerHTML += `<li>${data.response}</li>`
 
-        for (let i = 0; i < 10; i++) {
-            emailListEl.insertAdjacentHTML('beforeend', emailEl)
-        }
-    })
+        })
+}
